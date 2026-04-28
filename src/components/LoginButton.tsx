cdd92f980608aabdcf5379fb9f3384be3cf93b2d
Serigne Saliou Mbacke NIANG <saliou2755@gmail.com>
2026-04-28 11:10:58 +0000

    Composant LoginButton (Le Bouclier)

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function LoginButton() {
+  return (
+    <Link href="/login" className="bg-teal-600 text-white px-4 py-2 rounded-lg hover:bg-teal-700 transition" >
+      Se connecter
+    </Link>
+  );
+}
