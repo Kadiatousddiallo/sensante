9cb598a81402b5d4da624f8e8b4a5c2cefec633b
mame-penda-fall <mpf92005@gmail.com>
2026-05-05 19:36:09 +0000

    Fix structure projet Pilote + consultations

---
+++ src/app/consultations/page.tsx
@@ -0,0 +1,11 @@
+export default function ConsultationsPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">
+        Consultations
+      </h1>
+
+      <p>Page opérationnelle ✔</p>
+    </div>
+  );
+}
